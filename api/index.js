e4f279e6eb5084636e7164768e53ed20adb509ae
gaboolic <23441099@qq.com>
2023-07-14 21:32:36 +0800

    api

--- api/index.js
+++ api/index.js
@@ -2,10 +2,11 @@ const app = require('express')();
 const { v4 } = require('uuid');
 
 app.get('/api', (req, res) => {
+    const upgradeHeader = req.headers.get('Upgrade');
     const path = `/api/item/${v4()}`;
     res.setHeader('Content-Type', 'text/html');
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+    res.end(`Hello! Go to item: <a href="${upgradeHeader}">${upgradeHeader}</a>`);
 });
 
 app.get('/api/item/:slug', (req, res) => {
